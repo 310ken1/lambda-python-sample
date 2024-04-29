e3abeba6943caf15ac2230e67a17452bcdc6e682
Kenichi Sato <310dev@gmail.com>
2024-04-29 20:56:42 +0900

    hello:Parameter Storeから設定を取得するように修正

--- src/hello/handler.py
+++ src/hello/handler.py
@@ -1,18 +1,33 @@
 import pg8000.native
+import boto3
+import json
+
+
+def get_parameter(key):
+    """Get parameter from SSM
+    Args:
+        key (str): Parameter key
+    Returns:
+        dict: Parameter value
+    """
+    ssm = boto3.client('ssm')
+    param = ssm.get_parameter(Name=key)
+    return json.loads(param['Parameter']['Value'])
 
 
 def hello(event, context):
     try:
+        settings = get_parameter('Test')
         connect = pg8000.native.Connection(
-            host="localhost",
-            port="5432",
-            database="testdb",
-            user="postgres",
-            password="postgres")
+            host=settings['db']['host'],
+            port=settings['db']['port'],
+            database=settings['db']['database'],
+            user=settings['db']['user'],
+            password=settings['db']['password'])
         prepare = connect.prepare("SELECT * FROM data WHERE id=:id;")
         results = prepare.run(id=1)
-    except Exception:
-        print("Connection failed")
+    except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
     finally:
         print("Connection finally")
         prepare.close()
