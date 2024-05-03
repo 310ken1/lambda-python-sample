58f89b55372ff2c7fd79d5f1f733e6e351f5b8ea
Kenichi Sato <310dev@gmail.com>
2024-05-03 12:14:39 +0900

    スペルミス

--- src/goodbye/handler.py
+++ src/goodbye/handler.py
@@ -1,6 +1,6 @@
 
-def boodbye(event, context):
+def goodbye(event, context):
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "data": "boodbye"
+        "data": "goodbye"
     }
